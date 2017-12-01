9fd2bc04aa7e7947f9825b869379f7ef58abb256
Estudiantes <Estudiantes@labing50503.udistrital.edu.co>
2017-12-01 16:27:34 -0500

    comitiado4

---
+++ Formulario.php
@@ -0,0 +1,18 @@
+<html> 
+	<head> 
+	<title> Amados  XD  XP </title>
+ </head>
+
+    <body style= "background-color: firebrick">
+<main align="center"><big><big><big><b>Pareja enamorada <3<3<3<3</b></big></big></big></main>
+		<hr/>
+<div align="center"><img src="ama.jpg" height="300px" width="300px" alt=""/></div><br>
+<hr/>
+
+<form action="Procesar.php">
+            <big><center><input type="text" name="Sujeto1"></big>
+            <big><center><input type="text" name="Sujeto2"></big><br>
+            <center><input type="submit" >
+        </form>
+    </body>
+</html>
---
+++ Procesar.php
@@ -0,0 +1,20 @@
+
+
+<html> 
+	<head> 
+	<title> Amados  XD </title>
+ </head>
+<body style= "background-color: firebrick">
+
+<?php
+
+        $val1 = $_REQUEST["sujeto1"];
+        $val2 = $_REQUEST["sujeto2"];
+        $v1 = $val1 + $val2;
+        echo "Se aman :".$val1." ".$val2;
+
+?>
+
+   </body>
+</html>
+
